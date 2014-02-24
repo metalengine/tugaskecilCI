878ffd5631ae5af67dd2085e47e88003d88f2371
Rickard Elsen <ainx@ainx.gosc.info>
2014-02-24 20:35:08 +0700

    tambah kelas main,const,P,Q dan interface X

--- tugas-main/src/main/java/rpl/softwareconstruction/tugas/main/MainProg.java
+++ tugas-main/src/main/java/rpl/softwareconstruction/tugas/main/MainProg.java
@@ -6,10 +6,35 @@
 
 package rpl.softwareconstruction.tugas.main;
 
+import java.util.Vector;
+
 /**
  *
  * @author Rian Andrian
+ * @author Rickard Elsen
  */
-public class MainProg {
-    
+public class MainProg implements X{
+	public static void main(String[] args) {
+		int N = 10;
+		ABB a = new ABB();
+		
+		MainProg mainProg = new MainProg();
+		ABB a2 = new ABB(mainProg.w());
+		
+		Q q = new Q();
+		
+		a.f();
+		Vector<Integer> v = new Vector<Integer>(N);
+		v.add(a.getX());
+		v.add(a2.getX());
+		System.out.println("Isi Vector:");
+		for (Integer i: v) {
+			System.out.println(i);
+		}
+	}
+
+	@Override
+	public int w() {
+		return 2;
+	}    
 }
